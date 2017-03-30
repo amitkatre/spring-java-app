296bf879c946f136e0899abad3a5a1fd7f1dd20f
Amitkumar Katre <css2565@varian.com>
2017-03-30 12:04:21 -0700

    a

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        /*
+        
         final String baseUri = "http://localhost:"+(System.getenv("PORT")!=null?System.getenv("PORT"):"9998")+"/";
         final Map<String, String> initParams = new HashMap<String, String>();
         initParams.put("com.sun.jersey.config.property.packages","org.example.resources");
@@ -19,9 +19,9 @@ public class Main {
         System.out.println("Starting grizzly...");
         SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
         System.out.println(String.format("Jersey started with WADL available at %sapplication.wadl.",baseUri, baseUri));
-    	*/
-    	String pId = "a7x3C000000L0Jr";
+    	
+    	/*String pId = "a7x3C000000L0Jr";
 		String woId = "a1r3C000000L1hz";
-		InvoicePdfMergeController.mergeAttachmentControl(pId,woId);
+		InvoicePdfMergeController.mergeAttachmentControl(pId,woId);*/
     }
 }
